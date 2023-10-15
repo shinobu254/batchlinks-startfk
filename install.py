bf7ef3a33525cac75a6d3b6c0569d41b9e882ecb
RabuHoteru <41131937+shinobu254@users.noreply.github.com>
2023-10-15 15:18:30 -0400

    Fix Class

--- install.py
+++ install.py
@@ -1,9 +1,9 @@
-import launch
+import neko
 import platform
 
 if platform.system() == "Windows":
-    if not launch.is_installed("gdown"):
-        launch.run_pip("install gdown", "requirements for Batchlinks Download extension")
+    if not neko.is_installed("gdown"):
+        neko.run_pip("install gdown", "requirements for Batchlinks Download extension")
 
-    if not launch.is_installed("wget"):
-        launch.run_pip("install wget", "requirements for Batchlinks Download extension")
+    if not neko.is_installed("wget"):
+        neko.run_pip("install wget", "requirements for Batchlinks Download extension")
